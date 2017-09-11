b034b4f901e2c85688314d7e74e9549b762409d5
ramazanovar <ramazanovar@ufntc.ru>
2017-09-11 16:17:18 +0500

    refs #56797: Добавляет миграцию БД

--- DB/TelegramBotDBContext.cs
+++ DB/TelegramBotDBContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace GitlabTelegramBot.DB
 {
@@ -9,6 +12,23 @@ namespace GitlabTelegramBot.DB
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=GitlabBot.db");
+        }
+
+        public void Migrate()
+        {
+            var users = new List<TelegramBotUser>()
+            {
+                new TelegramBotUser() { ChatId  = 53290057, GitlabUserName = "RamazanovAR", TelegramName = "RamazanovAnvar"},
+                new TelegramBotUser() { ChatId  = 149901166, GitlabUserName = "MiryanovSN", TelegramName = "zzzzzzerg"},
+                new TelegramBotUser() { ChatId  = 103469385, GitlabUserName = "alyokhinaa", TelegramName = "zzanderss"},
+                new TelegramBotUser() { ChatId  = 293361925, GitlabUserName = "NugumanovDM", TelegramName = "hant111"},
+                new TelegramBotUser() { ChatId  = 93020512, GitlabUserName = "HolodnovJeJe", TelegramName = "HolodnovJeJe"},
+                new TelegramBotUser() { ChatId = 400611109, GitlabUserName = "GazimullinDM ", TelegramName = "GazimullinDM" },
+                new TelegramBotUser() {ChatId = 122671309, GitlabUserName = "kovalevasr", TelegramName = "jam_of_sweet" }
+            };
+
+            Users.AddRange(users);
+            SaveChanges();
         }
     }
 }
