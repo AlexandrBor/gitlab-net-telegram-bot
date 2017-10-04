eff4f6b906dd9b6a61ce078ee18db14004c9969c
ramazanovar <ramazanovar@ufntc.ru>
2017-10-04 10:29:11 +0500

    refs #57600: Добавляет в БД Ильдара

--- DB/TelegramBotDBContext.cs
+++ DB/TelegramBotDBContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace GitlabTelegramBot.DB
@@ -18,13 +16,14 @@ namespace GitlabTelegramBot.DB
         {
             var users = new List<TelegramBotUser>()
             {
-                new TelegramBotUser() { ChatId  = 53290057, GitlabUserName = "RamazanovAR", TelegramName = "RamazanovAnvar"},
-                new TelegramBotUser() { ChatId  = 149901166, GitlabUserName = "MiryanovSN", TelegramName = "zzzzzzerg"},
-                new TelegramBotUser() { ChatId  = 103469385, GitlabUserName = "alyokhinaa", TelegramName = "zzanderss"},
-                new TelegramBotUser() { ChatId  = 293361925, GitlabUserName = "NugumanovDM", TelegramName = "hant111"},
-                new TelegramBotUser() { ChatId  = 93020512, GitlabUserName = "HolodnovJeJe", TelegramName = "HolodnovJeJe"},
+                new TelegramBotUser() { ChatId = 53290057, GitlabUserName = "RamazanovAR", TelegramName = "RamazanovAnvar"},
+                new TelegramBotUser() { ChatId = 149901166, GitlabUserName = "MiryanovSN", TelegramName = "zzzzzzerg"},
+                new TelegramBotUser() { ChatId = 103469385, GitlabUserName = "alyokhinaa", TelegramName = "zzanderss"},
+                new TelegramBotUser() { ChatId = 293361925, GitlabUserName = "NugumanovDM", TelegramName = "hant111"},
+                new TelegramBotUser() { ChatId = 93020512, GitlabUserName = "HolodnovJeJe", TelegramName = "HolodnovJeJe"},
                 new TelegramBotUser() { ChatId = 400611109, GitlabUserName = "GazimullinDM ", TelegramName = "GazimullinDM" },
-                new TelegramBotUser() {ChatId = 122671309, GitlabUserName = "kovalevasr", TelegramName = "jam_of_sweet" }
+                new TelegramBotUser() { ChatId = 122671309, GitlabUserName = "kovalevasr", TelegramName = "jam_of_sweet" },
+                new TelegramBotUser() { ChatId = 202868276, GitlabUserName = "BashirovIA", TelegramName = "Bashirov_Ildar" }
             };
 
             Users.AddRange(users);
